c59bba4764bc0d56988565dc661990e33322a452
repiyann <muhrepiyan26@gmail.com>
2024-05-22 12:44:03 +0700

    feat(route): add new middlware route and provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,62 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/guest/Home'
-import Register from './pages/auth/Register'
-import Login from './pages/auth/Login'
+import Register from './pages/auth/user/Register'
+import Login from './pages/auth/user/Login'
+import Verifying from './pages/auth/user/Verify'
 import Forgot from './pages/auth/Forgot'
-import Verifying from './pages/auth/Verify'
-import Reset from './pages/auth/Reset'
+import Reset from './pages/auth/user/Reset'
 import Dashboard from './pages/user/Dashboard'
 import AdminDashboard from './pages/admin/AdminDashboard'
 import About from './pages/guest/About'
 import UserProfile from './pages/user/profile/UserProfile'
-import { useUserContext } from './utils/authProvider'
 import Product from './pages/user/Product'
+import UserRoute from './utils/user/UserRoute'
+import AdminRoute from './utils/admin/AdminRoute'
+import UserMiddleware from './utils/user/UserMiddleware'
+import AdminMiddleware from './utils/admin/AdminMiddleware'
+import LoginAdmin from './pages/auth/admin/LoginAdmin'
+import GuestMiddleware from './utils/guest/GuestMiddleware'
 
 function App() {
-	const { userData } = useUserContext()
-	const role = localStorage.getItem('userRole')
-
 	return (
 		<Routes>
-			{!userData && (
-				<>
-					<Route
-						path="/"
-						element={<Home />}
-					/>
-					<Route
-						path="/register"
-						element={<Register />}
-					/>
-					<Route
-						path="/login"
-						element={<Login />}
-					/>
-					<Route
-						path="/forgot"
-						element={<Forgot />}
-					/>
-					<Route
-						path="/verify"
-						element={<Verifying />}
-					/>
-					<Route
-						path="/reset"
-						element={<Reset />}
-					/>
-					<Route
-						path="/about"
-						element={<About />}
-					/>
-				</>
-			)}
-			<Route
-				path="/dashboard"
-				element={<>{userData?.role_id === 1 ? <AdminDashboard /> : <Dashboard />}</>}
-			/>
+			<Route element={<GuestMiddleware />}>
+				<Route
+					path="/register"
+					element={<Register />}
+				/>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/verify"
+					element={<Verifying />}
+				/>
+				<Route
+					path="/reset"
+					element={<Reset />}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+				<Route
+					path="/forgot"
+					element={<Forgot />}
+				/>
+			</Route>
 
-			{role === 'user' && (
-				<>
+			<Route element={<UserRoute />}>
+				<Route
+					path="/"
+					element={<Home />}
+				/>
+				<Route
+					path="/dashboard"
+					element={<Dashboard />}
+				/>
+				<Route element={<UserMiddleware />}>
 					<Route
 						path="/profile"
 						element={<UserProfile />}
@@ -65,13 +65,27 @@ function App() {
 						path="/detail/roses"
 						element={<Product />}
 					/>
-				</>
-			)}
+				</Route>
+			</Route>
+
+			<Route element={<AdminRoute />}>
+				<Route
+					path="/admin"
+					element={<LoginAdmin />}
+				/>
+				<Route element={<AdminMiddleware />}>
+					<Route
+						path="/admin/dashboard"
+						element={<AdminDashboard />}
+					/>
+				</Route>
+			</Route>
+
 			<Route
 				path="*"
 				element={
 					<Navigate
-						to="/dashboard"
+						to="/"
 						replace
 					/>
 				}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,9 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { UserProvider } from './utils/authService.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<BrowserRouter>
-		<UserProvider>
-			<App />
-		</UserProvider>
+		<App />
 	</BrowserRouter>
 )
