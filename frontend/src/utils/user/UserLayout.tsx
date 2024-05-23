911420f1ddf178da4da11b39448b0d0a7f4b0f6b
repiyann <muhrepiyan26@gmail.com>
2024-05-23 22:22:14 +0700

    fix(navbar): fix navbar rendering based on role

--- frontend/src/utils/user/UserLayout.tsx
+++ frontend/src/utils/user/UserLayout.tsx
@@ -1,14 +1,11 @@
-import { useUserContext } from './authProvider'
 import { Outlet } from 'react-router-dom'
 import Navbar from '@/components/Navbar'
-import NavbarUser from '@/components/NavbarUser'
 import Footer from '@/components/Footer'
 
 export default function UserLayout() {
-	const { userData } = useUserContext()
 	return (
 		<>
-			{userData ? <NavbarUser /> : <Navbar />}
+			<Navbar />
 			<Outlet />
 			<Footer />
 		</>
