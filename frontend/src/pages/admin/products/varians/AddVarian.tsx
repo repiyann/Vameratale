317a59cf607faf8562bcf5f0f8130d8737e6f875
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:28:13 +0700

    feat(input): add input to add varian data

--- frontend/src/pages/admin/products/varians/AddVarian.tsx
+++ frontend/src/pages/admin/products/varians/AddVarian.tsx
@@ -1,4 +1,6 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import NavbarAdmin from '@/components/NavbarAdmin'
@@ -6,8 +8,24 @@ import SidebarAdmin from '@/components/SidebarAdmin'
 import { Card } from '@/components/ui/card'
 
 export default function AddVarian() {
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>): void {
-		event.preventDefault()
+	const [varian, setVarian] = useState<string>('')
+	const [errorMessage, setErrorMessage] = useState<string>('')
+	const navigate = useNavigate()
+	const BASE_API_URL: string | undefined = process.env.REACT_APP_API_URL
+
+	async function handleSubmit(): Promise<void> {
+		try {
+			await axios.post(`${BASE_API_URL}/varian/createVarian`, {varian_name: varian})
+			navigate('/admin/products/varian')
+		} catch (error: unknown) {
+			if (axios.isAxiosError(error)) {
+				setErrorMessage(error.response?.data.message)
+			} else if (error instanceof Error) {
+				setErrorMessage(error.message)
+			} else {
+				setErrorMessage('Server bermasalah')
+			}
+		}
 	}
 
 	return (
@@ -35,11 +53,21 @@ export default function AddVarian() {
 						</div>
 						<h1 className="text-2xl font-bold">Tambah Varian</h1>
 						<Card className="p-5 mt-2 bg-[#FFF3F3]">
-							<form onSubmit={handleSubmit}>
+							<form
+								onSubmit={(e) => {
+									e.preventDefault()
+									handleSubmit()
+								}}
+							>
+								{errorMessage && <p className="text-red-500">{errorMessage}</p>}
 								<input
 									type="text"
 									className="border-2 w-full border-black mt-4 rounded-md px-2 py-2"
 									placeholder="Masukkan varian"
+									autoFocus
+									required
+									value={varian}
+									onChange={(e) => setVarian(e.target.value)}
 								/>
 								<div className="flex justify-center mt-4">
 									<button
