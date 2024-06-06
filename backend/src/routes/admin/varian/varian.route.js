f6b1aa94c5bf2d5ac057b85159bb5770dbc6bb14
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:20:51 +0700

    feat(route): add route for varian

---
+++ backend/src/routes/admin/varian/varian.route.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { createVarians, getVarians, getVarianByID, updateVarian, deleteVarian } from '../../../controllers/admin/varian/varian.controller.js'
+import pool from '../../../database/config.js'
+
+const router = express.Router()
+
+router.post('/createVarian', async (req, res, next) => await createVarians(req, res, pool, next))
+router.get('/getVarians', async (req, res, next) => await getVarians(req, res, pool, next))
+router.get('/getVarian/:id', async (req, res, next) => await getVarianByID(req, res, pool, next))
+router.put('/updateVarian/:id', async (req, res, next) => await updateVarian(req, res, pool, next))
+router.delete('/deleteVarian/:id', async (req, res, next) => await deleteVarian(req, res, pool, next))
+
+export default router
