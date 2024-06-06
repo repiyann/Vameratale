79af1291f53598551bf3ee3b0af5217fbd5aa9f8
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:20:02 +0700

    feat(controller): add fetch users controller for admin

---
+++ backend/src/controllers/admin/user/user.controller.js
@@ -0,0 +1,15 @@
+async function getUsersAdmin(req, res, pool, next) {
+	try {
+		const [results] = await pool.query('SELECT * FROM users')
+		if (results.length === 0) {
+			return res.status(404).json({ message: 'Pengguna tidak ada' })
+		}
+
+		return res.status(200).json({ message: 'Pengguna berhasil didapat', data: results })
+	} catch (error) {
+		console.error(error)
+		return res.status(500).json({ message: 'Server bermasalah' })
+	}
+}
+
+export { getUsersAdmin }
