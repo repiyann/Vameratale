c6d05400fb230ff229162fa837ca3bfe7e92be99
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:28:46 +0700

    refactor(navigation): fix some minor navigation

--- frontend/src/pages/admin/reports/ProductStock.tsx
+++ frontend/src/pages/admin/reports/ProductStock.tsx
@@ -1,6 +1,5 @@
-import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusSquare, faTrashCan } from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import NavbarAdmin from '@/components/NavbarAdmin'
 import SidebarAdmin from '@/components/SidebarAdmin'
@@ -22,19 +21,6 @@ export default function ProductStock() {
 				>
 					<div className="flex my-16 px-32 flex-col">
 						<h1 className="text-2xl font-bold">Stok Barang</h1>
-						<div className="mt-4">
-							<Link
-								to={'/admin/reports/stock/add-stock'}
-								className="px-4 pb-2 pt-1 bg-[#FAEAED] font-semibold rounded-xl border-[3px] border-[#D85A6D]"
-							>
-								<FontAwesomeIcon
-									icon={faPlusSquare}
-									size="xl"
-									className="text-[#606F49] mr-2"
-								/>
-								Tambah Stok
-							</Link>
-						</div>
 						<div className="mt-7">
 							<Table>
 								<TableHeader>
