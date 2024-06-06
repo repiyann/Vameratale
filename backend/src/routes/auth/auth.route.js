c68c27d0345427a2fb68b22f668d94785dc2cc4a
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:22:53 +0700

    fix(limiter): fix some rate limiter

--- backend/src/routes/auth/auth.route.js
+++ backend/src/routes/auth/auth.route.js
@@ -9,7 +9,7 @@ const router = express.Router()
 
 const attemptsLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
-	max: 5,
+	max: 25,
 	message: { message: 'Too many attempts. Please try again later.' },
 	standardHeaders: true,
 	legacyHeaders: false
@@ -19,10 +19,10 @@ router.post('/register', attemptsLimiter, async (req, res, next) => await regist
 router.post('/registerAdmin', attemptsLimiter, async (req, res, next) => await registerAdmin(req, res, pool, next))
 router.post('/login', attemptsLimiter, async (req, res, next) => await login(req, res, pool, next))
 router.post('/loginAdmin', attemptsLimiter, async (req, res, next) => await loginAdmin(req, res, pool, next))
-router.get('/getUser', verifyToken, async (req, res) => await getUser(req, res, pool))
-router.get('/getAdmin', verifyToken, async (req, res) => await getAdmin(req, res, pool))
+router.get('/getUser', verifyToken, async (req, res, next) => await getUser(req, res, pool, next))
+router.get('/getAdmin', verifyToken, async (req, res, next) => await getAdmin(req, res, pool, next))
 router.post('/generateOTP', attemptsLimiter, async (req, res, next) => await generateOTP(req, res, pool, next))
 router.post('/resetPasswordOTP', attemptsLimiter, async (req, res, next) => await resetPasswordOTP(req, res, pool, next))
-router.post('/verifyEmail', attemptsLimiter, async (req, res, next) => await verifyEmail(req, res, pool, next))
+router.post('/verifyEmail', async (req, res, next) => await verifyEmail(req, res, pool, next))
 
 export default router
