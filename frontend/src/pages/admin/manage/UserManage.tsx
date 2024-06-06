e26a7a55fad5e9adb3924bd31dc55ddfc7850649
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:25:58 +0700

    feat(UserManage): add user manage to fetch users data

--- frontend/src/pages/admin/manage/UserManage.tsx
+++ frontend/src/pages/admin/manage/UserManage.tsx
@@ -1,10 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import NavbarAdmin from '@/components/NavbarAdmin'
 import SidebarAdmin from '@/components/SidebarAdmin'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+type User = {
+	user_id: number
+	user_name: string
+	user_email: string
+	user_address: string
+	user_phone: string
+}
 
 export default function UserManage() {
+	const BASE_API_URL: string | undefined = process.env.REACT_APP_API_URL
+	const [users, setUsers] = useState<User[]>([])
+	const [errorMessage, setErrorMessage] = useState<string>('')
+
+	useEffect(() => {
+		const fetchData = async () => {
+			if (BASE_API_URL) {
+				try {
+					const response = await axios.get(`${BASE_API_URL}/get/getUsersAdmin`)
+					setUsers(response.data.data)
+				} catch (error: unknown) {
+					if (axios.isAxiosError(error)) {
+						setErrorMessage(error.response?.data.message)
+					} else if (error instanceof Error) {
+						setErrorMessage(error.message)
+					} else {
+						setErrorMessage('Server bermasalah')
+					}
+				}
+			}
+		}
+
+		fetchData()
+	}, [BASE_API_URL])
+
 	return (
 		<>
 			<NavbarAdmin />
@@ -21,31 +54,29 @@ export default function UserManage() {
 				>
 					<div className="flex my-16 px-32 flex-col">
 						<h1 className="text-2xl font-bold">Data Pelanggan</h1>
-						<div className='mt-3'>
+						<div className="mt-3">
 							<Table>
 								<TableHeader>
 									<TableRow>
+										<TableHead className="w-[10px]">No</TableHead>
 										<TableHead>Nama</TableHead>
-										<TableHead className="w-[200px]">No Telpon</TableHead>
-										<TableHead className="w-[200px]">Email</TableHead>
+										<TableHead>Email</TableHead>
+										<TableHead>Telepon</TableHead>
 										<TableHead>Alamat</TableHead>
 										<TableHead className="text-right">Aksi</TableHead>
 									</TableRow>
 								</TableHeader>
 								<TableBody>
-									<TableRow>
-										<TableCell className="font-medium">INV001</TableCell>
-										<TableCell>Paid</TableCell>
-										<TableCell>Credit Card</TableCell>
-										<TableCell>$250.00</TableCell>
-										<TableCell className="text-right">
-											<FontAwesomeIcon
-												icon={faTrashCan}
-												size="xl"
-												color="red"
-											/>
-										</TableCell>
-									</TableRow>
+									{errorMessage && <p className="text-red-500">{errorMessage}</p>}
+									{users.map((user, index) => (
+										<TableRow key={user.user_id}>
+											<TableCell className="font-medium">{index + 1}</TableCell>
+											<TableCell>{user.user_name}</TableCell>
+											<TableCell>{user.user_email}</TableCell>
+											<TableCell>{user.user_phone}</TableCell>
+											<TableCell>{user.user_address}</TableCell>
+										</TableRow>
+									))}
 								</TableBody>
 							</Table>
 						</div>
