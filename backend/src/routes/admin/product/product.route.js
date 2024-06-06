71185d4489bb13d3fd4b6e5a49dca67db95412ae
repiyann <muhrepiyan26@gmail.com>
2024-06-06 11:22:24 +0700

    refactor(file): move product route to approppriate place

---
+++ backend/src/routes/admin/product/product.route.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import {
+	createProduct,
+	getProducts,
+	getProductByID,
+	updateProduct,
+	deleteProduct
+} from '../../../controllers/admin/product/product.controller.js'
+import pool from '../../../database/config.js'
+
+const router = express.Router()
+
+router.post('/newProduct', async (req, res, next) => await createProduct(req, res, pool, next))
+router.get('/getProducts', async (req, res, next) => await getProducts(req, res, pool, next))
+router.get('/getProduct/:id', async (req, res, next) => await getProductByID(req, res, pool, next))
+router.put('/updateProduct/:id', async (req, res, next) => await updateProduct(req, res, pool, next))
+router.post('/deleteProduct/:id', async (req, res, next) => await deleteProduct(req, res, pool, next))
+
+export default router
