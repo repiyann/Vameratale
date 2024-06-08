6f199965a652a1967549a0e934e36a253e004925
repiyann <muhrepiyan26@gmail.com>
2024-06-08 19:55:49 +0700

    feat(crud): add crud for products table and upload images

--- backend/src/controllers/admin/product/product.controller.js
+++ backend/src/controllers/admin/product/product.controller.js
@@ -1,252 +1,209 @@
-async function createProduct(req, res) {
-	try {
-		await pool.beginTransaction()
+import validator from 'validator'
 
-		const { productName, productPrice, productDesc, category, variant, size, stock, image_path } = req.body
+async function createProduct(req, res, pool, next) {
+	const { productName, productPrice, productDesc, category, variant, size, stock } = req.body
 
-		if (!image_path) {
-			return res.status(400).json({ message: 'Image path is required' })
-		}
-
-		const newProduct = await pool.query(
-			`
-			INSERT INTO
-				products (product_name, product_price, product_description, product_category, product_variant, product_size, product_stock)
-			VALUES
-				(?, ?, ?, ?, ?, ?, ?)`,
-			[productName, productPrice, productDesc, category, variant, size, stock]
-		)
-
-		await pool.query(
-			`
-			INSERT INTO
-				product_images (product_id, image_path)
-			VALUES
-				(?, ?)`,
-			[newProduct.insertId, image_path]
-		)
-
-		await pool.commit()
-
-		return res.status(201).json({ message: 'Successfully created a new product', newProduct })
-	} catch (err) {
-		console.error(err.stack)
-		await pool.rollback()
-		res.status(500).json({ message: 'Internal Server Error' })
+	if (!req.files || !req.files.files || !Array.isArray(req.files.files)) {
+		return res.status(400).json({ message: 'No files uploaded or invalid format' })
 	}
-}
 
-async function getProducts(req, res) {
+	const uploadedFiles = Array.isArray(req.files.files) ? req.files.files : [req.files.files]
+	const connection = await pool.getConnection()
+
 	try {
-		await pool.beginTransaction()
+		await connection.beginTransaction()
 
-		const productQuery = 
-		`
-			SELECT
-				p.product_id AS productId,
-				p.product_name AS productName,
-				p.product_price AS productPrice,
-				p.product_description AS productDesc,
-				p.product_category AS category,
-				p.product_variant AS variant,
-				p.product_size AS size,
-				p.product_stock AS stock,
-				pi.image_path AS imagePath 
-			FROM
-				products p
-			LEFT JOIN
-				product_images pi
-			ON
-				p.product_id = pi.image_product_id
-		`
+		const insertProduct = `
+			INSERT INTO 
+				products 
+					(product_name, product_price, product_description, product_category, product_variant, product_size, product_stock)
+			VALUES
+				(?, ?, ?, ?, ?, ?, ?)`
 
-		const productsResult = await pool.query(productQuery)
+		const [productResult] = await connection.execute(insertProduct, [
+			productName,
+			productPrice,
+			productDesc,
+			category,
+			variant,
+			size,
+			stock
+		])
+		const productId = productResult.insertId
 
-		const productsMap = {}
-		productsResult.forEach((product) => {
-			if (!productsMap[product.productId]) {
-				productsMap[product.productId] = {
-					productId: product.productId,
-					productName: product.productName,
-					productPrice: product.productPrice,
-					productDesc: product.productDesc,
-					category: product.category,
-					variant: product.variant,
-					size: product.size,
-					stock: product.stock,
-					images: []
-				}
-			}
-			if (product.imagePath) {
-				productsMap[product.productId].images.push(product.imagePath)
-			}
+		const insertImage = uploadedFiles.map(async (file) => {
+			const fileData = file.data
+			const query = 'INSERT INTO images_tes (product_id, data) VALUES (?, ?)'
+
+			await connection.execute(query, [productId, fileData])
 		})
 
-		const result = Object.values(productsMap)
-		
-		await pool.commit()
-		
-		return res.status(200).json({ message: 'Products fetched successfully', products: result })
-	} catch (err) {
-		console.error(err.stack)
-		await pool.rollback()
-		res.status(500).json({ message: 'Internal Server Error' })
+		await Promise.all(insertImage)
+		await connection.commit()
+
+		return res.status(201).json({ message: 'Produk berhasil dibuat' })
+	} catch (error) {
+		await connection.rollback()
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah', error: error.message })
+	} finally {
+		connection.release()
 	}
 }
 
-async function getProductByID(req, res) {
-	try {
-		await pool.beginTransaction()
+async function getProducts(req, res, pool, next) {
+	const connection = await pool.getConnection()
 
-		const productId = req.params.id
-		const productQuery = 
-		`
+	try {
+		const query = `
 			SELECT 
-				p.product_id AS productId,
-				p.product_name AS productName,
-				p.product_price AS productPrice,
-				p.product_description AS productDesc,
-				p.product_category AS category,
-				p.product_variant AS variant,
-				p.product_size AS size,
-				p.product_stock AS stock,
-				pi.image_path AS imagePath
+				p.product_id, 
+				p.product_name, 
+				p.product_price, 
+				p.product_description, 
+				p.product_category, 
+				p.product_variant, 
+				p.product_size, 
+				p.product_stock, 
+				i.data AS image_data 
 			FROM 
 				products p
-			LEFT JOIN 
-				product_images pi
-			ON 
-				p.product_id = pi.image_product_id
-			WHERE 
-				p.product_id = ?
-		`
+			LEFT JOIN (
+ 				SELECT 
+					id, product_id, data
+ 				FROM 
+					images_tes
+ 				WHERE 
+					(product_id, id) IN (
+		 				SELECT
+							product_id, MAX(id) AS max_id
+		 				FROM
+							images_tes
+		 				GROUP BY
+							product_id
+ 				)
+			) i ON p.product_id = i.product_id`
 
-		const productResult = await pool.query(productQuery, [productId])
-
-		if (productResult.length === 0) {
-			return res.status(404).json({ message: 'Product not found' })
-		}
-
-		const product = {
-			productId: productResult[0].productId,
-			productName: productResult[0].productName,
-			productPrice: productResult[0].productPrice,
-			productDesc: productResult[0].productDesc,
-			category: productResult[0].category,
-			variant: productResult[0].variant,
-			size: productResult[0].size,
-			stock: productResult[0].stock,
-			images: []
-		}
-
-		productResult.forEach((row) => {
-			if (row.imagePath) {
-				product.images.push(row.imagePath)
-			}
+		const [rows] = await connection.execute(query)
+		rows.forEach((row) => {
+			row.image_data = row.image_data.toString('base64')
 		})
 
-		await pool.commit()
-
-		return res.status(200).json({ message: 'Successfully fetched product', product })
-	} catch (err) {
-		console.error(err.stack)
-		await pool.rollback()
-		res.status(500).json({ message: 'Internal Server Error' })
+		return res.status(200).json({ message: 'Berhasil mengambil semua produk', rows })
+	} catch (error) {
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah', error: error.message })
+	} finally {
+		connection.release()
 	}
 }
 
-async function updateProduct(req, res) {
+async function getProductByID(req, res, pool, next) {
+	const productId = req.params.id
+
+	if (!validator.isInt(productId, { min: 1 })) {
+		return res.status(400).json({ message: 'ID produk tidak valid' })
+	}
+
+	const connection = await pool.getConnection()
+
 	try {
-		await pool.beginTransaction()
+		const productQuery = `SELECT * FROM products WHERE product_id = ?`
+		const [productRows] = await connection.execute(productQuery, [productId])
 
-		const productId = req.params.id
-		const { productName, productPrice, productDesc, category, variant, size, stock, images } = req.body
-		const productQuery = 
-		`
-			UPDATE
-				products 
-			SET
-				product_name = ?,
-				product_price = ?,
-				product_description = ?,
-				product_category = ?,
-				product_variant = ?,
-				product_size = ?,
-				product_stock = ?
-			WHERE 
-				product_id = ?
-		`
-
-		await pool.query(productQuery, [productName, productPrice, productDesc, category, variant, size, stock, productId])
-
-		await pool.query(
-			`
-			DELETE FROM 
-				product_images 
-			WHERE
-				image_product_id = ?`,
-			[productId]
-		)
-
-		if (images && images.length > 0) {
-			const imageInsertPromises = images.map((imagePath) => {
-				return pool.query(
-					`
-					INSERT INTO 
-						product_images (image_product_id, image_path)
-					VALUES
-						(?, ?)`,
-					[productId, imagePath]
-				)
-			})
-			await Promise.all(imageInsertPromises)
+		if (productRows.length === 0) {
+			return res.status(404).json({ message: 'Produk tidak ditemukan' })
 		}
 
-		await pool.commit()
+		const imagesQuery = `SELECT * FROM images_tes WHERE product_id = ?`
+		const [imagesRows] = await connection.execute(imagesQuery, [productId])
 
-		return res.status(200).json({ message: 'Successfully updated the product' })
-	} catch (err) {
-		console.error(err.stack)
-		await pool.rollback()
-		res.status(500).json({ message: 'Internal Server Error' })
+		const product = productRows[0]
+		product.images = imagesRows
+
+		return res.status(200).json({ message: 'Berhasil fetch produk', product })
+	} catch (error) {
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah', error: error.message })
+	} finally {
+		connection.release()
 	}
 }
 
-async function deleteProduct(req, res) {
-	try {
-		await pool.beginTransaction()
+async function updateProduct(req, res, pool, next) {
+	const productId = req.params.id
+	const { productName, productPrice, productDesc, category, variant, size, stock, images } = req.body
 
+	if (!validator.isInt(productId, { min: 1 })) {
+		return res.status(400).json({ message: 'Invalid product ID' })
+	}
+
+	const connection = await pool.getConnection()
+
+	try {
+		await connection.beginTransaction()
+
+		const productQuery = `
+      UPDATE
+        products 
+      SET
+        product_name = ?,
+        product_price = ?,
+        product_description = ?,
+        product_category = ?,
+        product_variant = ?,
+        product_size = ?,
+        product_stock = ?
+      WHERE 
+        product_id = ?`
+
+		await connection.execute(productQuery, [
+			productName,
+			productPrice,
+			productDesc,
+			category,
+			variant,
+			size,
+			stock,
+			productId
+		])
+
+		await connection.execute(`DELETE FROM images_tes WHERE product_id = ?`, [productId])
+
+		if (images && images.length > 0) {
+			const updateImage = images.map((imageData) => {
+				return connection.execute(`INSERT INTO images_tes (product_id, data) VALUES (?, ?)`, [productId, imageData])
+			})
+			await Promise.all(updateImage)
+		}
+
+		await connection.commit()
+		return res.status(200).json({ message: 'Successfully updated the product' })
+	} catch (error) {
+		await connection.rollback()
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah', error: error.message })
+	} finally {
+		connection.release()
+	}
+}
+
+async function deleteProduct(req, res, pool, next) {
+	try {
 		const productId = req.params.id
 
-		await pool.query(
-			`
-			DELETE FROM
-				product_images 
-			WHERE
-			image_product_id = ?`,
-			[productId]
-		)
+		await pool.execute(`DELETE FROM images_tes WHERE product_id = ?`, [productId])
 
-		const deleteResult = await pool.query(
-			`
-			DELETE FROM
-				products 
-			WHERE
-				product_id = ?`,
-			[productId]
-		)
+		const deleteResult = await pool.execute(`DELETE FROM products WHERE product_id = ?`, [productId])
 
 		if (deleteResult.affectedRows === 0) {
 			return res.status(404).json({ message: 'Product not found' })
 		}
 
-		await pool.commit()
-
 		return res.status(200).json({ message: 'Successfully deleted the product' })
-	} catch (err) {
-		console.error(err.stack)
-		await pool.rollback()
-		res.status(500).json({ message: 'Internal Server Error' })
+	} catch (error) {
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah' })
 	}
 }
 
