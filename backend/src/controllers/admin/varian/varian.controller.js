76fb1270579790ff7c1b2166cff6a6e780f8c015
repiyann <muhrepiyan26@gmail.com>
2024-06-09 21:23:20 +0700

    feat(get): add get varian based on selected category

--- backend/src/controllers/admin/varian/varian.controller.js
+++ backend/src/controllers/admin/varian/varian.controller.js
@@ -2,13 +2,22 @@ import validator from 'validator'
 
 async function createVarians(req, res, pool, next) {
 	try {
-		const { varian_name } = req.body
+		const { varian_name, category_id } = req.body
 
 		if (validator.isEmpty(varian_name)) {
 			return res.status(400).json({ message: 'Input tidak boleh kosong' })
 		}
 
-		await pool.query('INSERT INTO varians (varian_name) VALUES (?)', [varian_name])
+		if (!validator.isInt(category_id, { min: 1 })) {
+			return res.status(400).json({ message: 'Kategori tidak boleh kosong' })
+		}
+
+		const categoryNumber = Number(category_id)
+
+		await pool.query('INSERT INTO varians (varian_name, varian_category_id) VALUES (?, ?)', [
+			varian_name,
+			categoryNumber
+		])
 
 		return res.status(201).json({ message: 'Varian berhasil dibuat' })
 	} catch (error) {
@@ -19,7 +28,26 @@ async function createVarians(req, res, pool, next) {
 
 async function getVarians(req, res, pool, next) {
 	try {
-		const [results] = await pool.query('SELECT * FROM varians')
+		const [results] = await pool.execute(`SELECT varians.varian_id, varians.varian_name, categories.category_name
+			FROM varians
+			INNER JOIN categories ON varians.varian_category_id = categories.category_id`)
+		if (results.length === 0) {
+			return res.status(404).json({ message: 'Varian kosong' })
+		}
+
+		return res.status(200).json({ message: 'Varian berhasil didapat', data: results })
+	} catch (error) {
+		console.error(error.stack)
+		return res.status(500).json({ message: 'Server bermasalah' })
+	}
+}
+
+async function getVarian(req, res, pool, next) {
+	try {
+		const { id } = req.params
+		const [results] = await pool.query(`SELECT varian_id, varian_name FROM varians WHERE varian_category_id = ?`, [
+			id
+		])
 		if (results.length === 0) {
 			return res.status(404).json({ message: 'Varian kosong' })
 		}
@@ -34,9 +62,9 @@ async function getVarians(req, res, pool, next) {
 async function updateVarian(req, res, pool, next) {
 	try {
 		const { id } = req.params
-		const { varian_name } = req.body
+		const { varian_name, category_id } = req.body
 
-		if (!validator.isInt(id, { min: 1 })) {
+		if (!validator.isInt(id, { min: 1 }) || !validator.isInt(category_id, { min: 1 })) {
 			return res.status(400).json({ message: 'ID harus angka' })
 		}
 
@@ -44,12 +72,17 @@ async function updateVarian(req, res, pool, next) {
 			return res.status(400).json({ message: 'Input tidak boleh kosong' })
 		}
 
+		const categoryNumber = Number(category_id)
+
 		const [checkNotes] = await pool.query('SELECT * FROM varians WHERE varian_id = ?', [id])
 		if (checkNotes.length === 0) {
 			return res.status(404).json({ message: 'Varian tidak ditemukan' })
 		}
 
-		const [results] = await pool.query('UPDATE varians SET varian_name = ? WHERE varian_id = ?', [varian_name, id])
+		const [results] = await pool.query(
+			'UPDATE varians SET varian_name = ?, varian_category_id = ? WHERE varian_id = ?',
+			[varian_name, categoryNumber, id]
+		)
 		if (results.affectedRows === 0) {
 			return res.status(200).json({ message: 'Tidak ada perubahan' })
 		}
@@ -86,4 +119,4 @@ async function deleteVarian(req, res, pool, next) {
 	}
 }
 
-export { createVarians, getVarians, updateVarian, deleteVarian }
+export { createVarians, getVarians, getVarian, updateVarian, deleteVarian }
--- backend/src/routes/admin/varian/varian.route.js
+++ backend/src/routes/admin/varian/varian.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createVarians, getVarians, updateVarian, deleteVarian } from '../../../controllers/admin/varian/varian.controller.js'
+import { createVarians, getVarians, getVarian, updateVarian, deleteVarian } from '../../../controllers/admin/varian/varian.controller.js'
 import pool from '../../../database/config.js'
 
 const router = express.Router()
 
 router.post('/createVarian', async (req, res, next) => await createVarians(req, res, pool, next))
 router.get('/getVarians', async (req, res, next) => await getVarians(req, res, pool, next))
+router.get('/getVarian/:id', async (req, res, next) => await getVarian(req, res, pool, next))
 router.put('/updateVarian/:id', async (req, res, next) => await updateVarian(req, res, pool, next))
 router.delete('/deleteVarian/:id', async (req, res, next) => await deleteVarian(req, res, pool, next))
 
