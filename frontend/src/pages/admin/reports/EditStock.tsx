b0a717ec6a5c597465fbe2392ab5200959b54a28
repiyann <muhrepiyan26@gmail.com>
2024-06-10 16:29:08 +0700

    feat(edit): add edit stock page

--- frontend/src/pages/admin/reports/EditStock.tsx
+++ frontend/src/pages/admin/reports/EditStock.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 import NavbarAdmin from '@/components/NavbarAdmin'
 import SidebarAdmin from '@/components/SidebarAdmin'
 import { Card } from '@/components/ui/card'
 
-export default function AddStock() {
+export default function EditStock() {
 	const [stock, setStock] = useState<string>('')
 	const [errorMessage, setErrorMessage] = useState<string>('')
 	const navigate = useNavigate()
+	const { id } = useParams()
 	const BASE_API_URL: string | undefined = process.env.REACT_APP_API_URL
 	const token: string | null = sessionStorage.getItem('token')
 
+	function handleAxiosError(error: unknown): void {
+		if (axios.isAxiosError(error)) {
+			setErrorMessage(error.response?.data.message)
+		} else if (error instanceof Error) {
+			setErrorMessage(error.message)
+		} else {
+			setErrorMessage('Server bermasalah')
+		}
+	}
+
+	useEffect(() => {
+		async function fetchStockData(): Promise<void> {
+			try {
+				const response = await axios.get(`${BASE_API_URL}/stock/getStock/${id}`, {
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
+				})
+
+				setStock(response.data.data)
+			} catch (error) {
+				handleAxiosError(error)
+			}
+		}
+
+		fetchStockData()
+	}, [BASE_API_URL, id, token])
+
 	async function handleSubmit(): Promise<void> {
 		try {
-			await axios.post(`${BASE_API_URL}/stock/addStock`, {stock: stock}, {
-				headers: {
-					Authorization: `Bearer ${token}`
+			await axios.put(
+				`${BASE_API_URL}/stock/updateStock/${id}`,
+				{ stock },
+				{
+					headers: {
+						Authorization: `Bearer ${token}`
+					}
 				}
-			})
+			)
+			
 			navigate('/admin/reports/stock')
 		} catch (error: unknown) {
-			if (axios.isAxiosError(error)) {
-				setErrorMessage(error.response?.data.message)
-			} else if (error instanceof Error) {
-				setErrorMessage(error.message)
-			} else {
-				setErrorMessage('Server bermasalah')
-			}
+			handleAxiosError(error)
 		}
 	}
 
@@ -50,7 +78,7 @@ export default function AddStock() {
 					<div className="flex my-16 px-32 flex-col">
 						<div>
 							<Link
-								to={'/admin/products/varian'}
+								to={'/admin/reports/stock'}
 								className="px-2 py-1 rounded-2xl border-2 border-[#DC9D9D] text-[#990000] font-semibold"
 							>
 								<FontAwesomeIcon icon={faChevronLeft} /> Kembali
@@ -66,7 +94,7 @@ export default function AddStock() {
 							>
 								{errorMessage && <p className="text-red-500">{errorMessage}</p>}
 								<input
-									type="text"
+									type="number"
 									className="border-2 w-full border-black mt-4 rounded-md px-2 py-2"
 									placeholder="Masukkan stock"
 									autoFocus
